d5212d6dfbe8b1136e3b0c0f2d25dfb1f2374368
matheecs <matheecs@qq.com>
2021-09-06 17:06:45 +0800

    fix some bugs

--- src/constraint/FootPlacementConstraint.cpp
+++ src/constraint/FootPlacementConstraint.cpp
@@ -64,9 +64,11 @@ vector_t FootPlacementConstraint::getValue(
   // Step 4 find a closest region (A,b)
   scalar_t xEE = pEEReference(0);
   scalar_t yEE = pEEReference(1);
-  scalar_t terrainIndex = std::round(xEE / terrainGap_);
-  scalar_t xMin = terrainIndex * terrainGap_ - terrainSizeX_ / 2;
-  scalar_t XMax = terrainIndex * terrainGap_ + terrainSizeX_ / 2;
+
+  const scalar_t terrainGap = 0.3;
+  scalar_t terrainIndex = std::round(xEE / terrainGap);
+  scalar_t xMin = terrainIndex * terrainGap - terrainSizeX_ / 2;
+  scalar_t XMax = terrainIndex * terrainGap + terrainSizeX_ / 2;
   scalar_t yMin = 0 - terrainSizeY_ / 2;
   scalar_t yMax = 0 + terrainSizeY_ / 2;
   Eigen::Matrix<scalar_t, 4, 3> A;
@@ -84,6 +86,8 @@ vector_t FootPlacementConstraint::getValue(
 
   // Step 6 add constraint Ax + b + s >=0
   vector3_t pEE = endEffectorKinematicsPtr_->getPosition(state).front();
+
+  std::cout << "Value = \n" << A * pEE + b + s << "\n";
   return A * pEE + b + s;
 }
 
@@ -91,19 +95,9 @@ VectorFunctionLinearApproximation
 FootPlacementConstraint::getLinearApproximation(
     scalar_t time, const vector_t& state, const vector_t& input,
     const PreComputation& preComp) const {
-  // TODO
-  // f(x,u) = dfdx dx + dfdu du + f
-  VectorFunctionLinearApproximation linearApproximation =
-      VectorFunctionLinearApproximation::Zero(getNumConstraints(time),
-                                              state.size(), input.size());
-  Eigen::Matrix<scalar_t, 4, 3> A;
-  A << 1, 0, 0, -1, 0, 0, 0, 1, 0, 0, -1, 0;
-  const auto positionApprox =
-      endEffectorKinematicsPtr_->getPositionLinearApproximation(state).front();
-
-  linearApproximation.f.noalias() = positionApprox.f;
-  linearApproximation.dfdx.noalias() = A * positionApprox.dfdx;
-  return linearApproximation;
+  throw std::runtime_error(
+      "[FootPlacementConstraint::getLinearApproximation] Linear approximation "
+      "not implemented!");
 }
 
 VectorFunctionQuadraticApproximation
@@ -111,14 +105,74 @@ FootPlacementConstraint::getQuadraticApproximation(
     scalar_t time, const vector_t& state, const vector_t& input,
     const PreComputation& preComp) const {
   // TODO
+  // Step 0 get start (current) time
+  const scalar_t trotGaitWholeCycle = 0.7;
+  const scalar_t currentTime = referenceManagerPtr_->getCurrentTime();
+  assert(time >= currentTime);
+  assert(time <= currentTime + trotGaitWholeCycle);
+
+  // Step 1 get middle of the last stance phase
+  scalar_t middle =
+      referenceManagerPtr_->getGaitSchedule()->getMiddleOfLastStance(time);
+
+  // Step 2 get target state
+  const TargetTrajectories& targetTrajectories =
+      referenceManagerPtr_->getTargetTrajectories();
+  const vector_t stateReference = targetTrajectories.getDesiredState(middle);
+
+  // Step 3 get target end-effector position
+  assert(endEffectorKinematicsPtr_->getIds().size() == 1);
+  vector3_t pEEReference =
+      endEffectorKinematicsPtr_->getPosition(stateReference).front();
+
+  // Step 4 find a closest region (A,b)
+  scalar_t xEE = pEEReference(0);
+  scalar_t yEE = pEEReference(1);
+
+  const scalar_t terrainGap = 0.3;
+  scalar_t terrainIndex = std::round(xEE / terrainGap);
+  scalar_t xMin = terrainIndex * terrainGap - terrainSizeX_ / 2;
+  scalar_t XMax = terrainIndex * terrainGap + terrainSizeX_ / 2;
+  scalar_t yMin = 0 - terrainSizeY_ / 2;
+  scalar_t yMax = 0 + terrainSizeY_ / 2;
   Eigen::Matrix<scalar_t, 4, 3> A;
   A << 1, 0, 0, -1, 0, 0, 0, 1, 0, 0, -1, 0;
-  // f[i](x,u) = 1/2 dx' dfdxx[i] dx + du' dfdux[i] dx + 1/2 du' dfduu[i] du
-  //             + dfdx[i,:] dx + dfdu[i,:] du + f[i]
+  Eigen::Matrix<scalar_t, 4, 1> b;
+  b << -xMin, XMax, -yMin, yMax;
+
+  // Step 5 s = get swing time left (0.35 -> 0)
+  scalar_t swingTimeLeft =
+      referenceManagerPtr_->getGaitSchedule()->getSwingTimeLeft();
+  assert(swingTimeLeft >= 0);
+  assert(swingTimeLeft <= trotGaitWholeCycle / 2);
+  Eigen::Matrix<scalar_t, 4, 1> s;
+  s << swingTimeLeft, swingTimeLeft, swingTimeLeft, swingTimeLeft;
+
+  VectorFunctionQuadraticApproximation positionQuadraticApproximation =
+      endEffectorKinematicsPtr_->getPositionQuadraticApproximation(state)
+          .front();
   VectorFunctionQuadraticApproximation quadraticApproximation;
-  quadraticApproximation.f.noalias() = A;
-  quadraticApproximation.dfdx.noalias() = A;
-  quadraticApproximation.dfdxx.noalias() = A;
+  quadraticApproximation.f = A * positionQuadraticApproximation.f + b + s;
+  quadraticApproximation.dfdx = A * positionQuadraticApproximation.dfdx;
+
+  quadraticApproximation.dfdxx.emplace_back(
+      1 * positionQuadraticApproximation.dfdxx[0]);
+  quadraticApproximation.dfdxx.emplace_back(
+      -1 * positionQuadraticApproximation.dfdxx[0]);
+  quadraticApproximation.dfdxx.emplace_back(
+      1 * positionQuadraticApproximation.dfdxx[1]);
+  quadraticApproximation.dfdxx.emplace_back(
+      -1 * positionQuadraticApproximation.dfdxx[1]);
+
+  std::cout << "quadraticApproximation.f = \n"
+            << quadraticApproximation.f << std::endl;
+  std::cout << "quadraticApproximation.dfdx = \n"
+            << quadraticApproximation.dfdx << std::endl;
+  std::cout << "quadraticApproximation.dfdxx[0] = \n"
+            << quadraticApproximation.dfdxx[0] << std::endl;
+  std::cout << "quadraticApproximation.dfdxx.size() = \n"
+            << quadraticApproximation.dfdxx.size() << std::endl;
+
   return quadraticApproximation;
 };
 
